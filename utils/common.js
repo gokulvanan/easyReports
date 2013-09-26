4a5477b7fd9b5ce6655aea5570775f4104493a74
gokulvanan <gokulvanan@gmail.com>
2013-09-26 19:23:44 +0530

    reorganized package to make framework installable and ready to use

--- utils/common.js
+++
@@ -1,7 +0,0 @@
-
-
-exports.getJoinFlag = function(row,join,delim){
-	var output = new Array();
-	for(var j in join)	output.push(row[join[j]]);
-	return output.join(delim);//TODO have this delimiter customizable through application.conf
-}
