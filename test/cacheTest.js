f7cd0ad92a296ed303344cd37426a724831763f0
gokulvanan <gokulvanan@gmail.com>
2013-11-21 08:37:44 +0530

    added new test cases moved cache to wrapper file

---
+++ test/cacheTest.js
@@ -0,0 +1,38 @@
+
+
+var cache = require("../lib/utils/cache.js");
+
+module.exports = {
+  setUp: function (callback) {
+   console.log("setup call");
+   this.conf = require("/home/komlimobile/git/KM-Interface-Components/kmreports/config.js")
+   callback();
+  },
+  tearDown: function (callback) {
+    console.log("tearDown call");
+    this.conf = null;
+    callback();
+  },
+  initTest: function (test) {
+    cache.init(this.conf);
+    //TODO mock and stub logger to verify the log printed
+    test.done();
+  },
+  setTest: function (test) {
+    test.expect(1);
+    cache.init(this.conf);
+    cache.getCache("list").set("list","test",function(err,data){
+      test.isNull(err);
+      test.done();  
+    },10);
+  },
+  getTest: function (test) {
+    test.expect(2);
+    cache.init(this.conf);
+    cache.getCache("list").get(function(err,data){
+      test.isNull(err);
+      test.equasl(data,"test");
+      test.done();  
+    });
+  }
+};
